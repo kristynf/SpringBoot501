1fea4b1af8b4e59c6043452a46f58166752c23e6
kristynf <kristynf@bu.edu>
2020-03-02 11:09:07 -0500

    initial commit

---
+++ src/test/java/com/kristyn/springboot501/Springboot501ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristyn.springboot501;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot501ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
